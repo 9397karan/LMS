9f2bc4e19a9ef90d875bcc5fae6998ee40a16d85
9397karan <144720736+9397karan@users.noreply.github.com>
2025-02-02 20:13:37 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://lms-front-tl73.onrender.com",
     credentials: true,
   })
 );
